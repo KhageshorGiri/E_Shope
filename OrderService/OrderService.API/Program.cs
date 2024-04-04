db29c47949dd5f3bf950911523fff55568e3acdf
KhageshorGiri <girikhageshor2432@gmail.com>
2024-04-04 20:30:59 +0545

    Added health check

--- OrderService/OrderService.API/Program.cs
+++ OrderService/OrderService.API/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using OrderService.Infrastructure.Data;
 using Serilog;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,11 @@ try
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+
+    //health checks
+    builder.Services.AddHealthChecks()
+        .AddNpgSql(builder.Configuration.GetConnectionString("PostgraceServerConnection"));
+
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
@@ -42,6 +50,29 @@ try
     app.UseAuthorization();
 
     app.MapControllers();
+
+    // Health check Middleware configuration
+    app.MapHealthChecks("/health", new HealthCheckOptions()
+    {
+        ResponseWriter = async (context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        name = entry.Key.ToString(),
+                        status = entry.Value.Status.ToString(),
+                        exception = entry.Value.Exception is not null ? entry.Value.Exception.Message : "none",
+                        duration = entry.Value.Duration.ToString()
+                    })
+                });
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
 
     app.Run();
 
