186188d2b1c191330753bef75c134ebd0bdeb657
KhageshorGiri <girikhageshor2432@gmail.com>
2024-04-08 20:48:50 +0545

    Added Authorization filter for Order Service

---
+++ OrderService/OrderService.API/Filters/JwtAuthorizationAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace OrderService.API.Filters
+{
+
+    public class JwtAuthorizationAttribute : Attribute, IAuthorizationFilter
+    {
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+
+            // Extract JWT token from request headers
+            string token = context.HttpContext.Request.Headers["Authorization"].ToString();
+
+            if (string.IsNullOrEmpty(token))
+            {
+                context.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            // Make request to authentication service to validate token
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", token);
+                var authServiceUrl = "http://localhost:5255" + "/api/TokenValidator";
+                var authResponse = httpClient.GetAsync(authServiceUrl).Result;
+
+                if (!authResponse.IsSuccessStatusCode)
+                {
+                    context.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
+                    return;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ OrderService/OrderService.Infrastructure/Migrations/20240408150036_initialSetup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    OrderDescription = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OrderDeliveryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedBy = table.Column<int>(type: "integer", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
